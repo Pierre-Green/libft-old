f3102e378b655841abdecd86fe054c82a261be1c
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-11-07 19:43:47 +0100

    Whheee

---
+++ ft_strnequ.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 19:40:34 by pguthaus          #+#    #+#             */
+/*   Updated: 2018/11/07 19:42:39 by pguthaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	int	current;
+
+	current = -1;
+	while ((size_t)++current < n)
+		if (s1[current] != s2[current])
+			return (0);
+	return (1);
+}
