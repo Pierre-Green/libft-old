5f36de3f44080985255c741d170e14dbba9d737b
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-11-08 19:11:01 +0100

    Iteritera

---
+++ ft_striteri.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/08 19:07:42 by pguthaus          #+#    #+#             */
+/*   Updated: 2018/11/08 19:08:33 by pguthaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void				ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	current;
+
+	current = 0;
+	while (s[current])
+	{
+		f(current, &s[current]);
+		current++;
+	}
+}
