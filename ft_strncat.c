27b84762679e1ab12e96c6538f227335d173be64
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-11-08 16:30:54 +0100

    nmi

---
+++ ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 15:26:25 by pguthaus          #+#    #+#             */
+/*   Updated: 2018/11/08 16:28:55 by pguthaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strncat(char *dest, const char *src, size_t n)
+{
+	size_t	current;
+	int		d_length;
+
+	current = 0;
+	d_length = ft_strlen(dest);
+	while (current < n && src[current] != '\0')
+	{
+		dest[d_length + current] = src[current];
+		current++;
+	}
+	dest[d_length + current] = '\0';
+	return (dest);
+}
