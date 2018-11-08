c6d62e34ab222d33c0180bf87fb6e65307b837a2
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-11-08 19:04:01 +0100

    Better

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,21 +6,17 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 18:22:16 by pguthaus          #+#    #+#             */
-/*   Updated: 2018/11/08 18:24:48 by pguthaus         ###   ########.fr       */
+/*   Updated: 2018/11/08 19:03:19 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *  	ft_strnew(size_t size)
+char 		*ft_strnew(size_t size)
 {
 	char	*res;
-	size_t	current;
-
-	if (!(res = (char *)malloc((1 + size) * sizeof(char))))
+	if (!(res = (char *)ft_memalloc(size + 1)))
 		return (NULL);
-	current = -1;
-	while (++current <= size)
-		res[current] = '\0';
+	ft_strclr(res);
 	return (res);
 }
