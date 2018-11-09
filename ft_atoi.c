d3e1f2f945a617b442971805aa5258a0d838c1f4
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-11-09 18:48:45 +0100

    Ail Toz

---
+++ ft_atoi.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 15:27:45 by pguthaus          #+#    #+#             */
+/*   Updated: 2018/11/09 18:47:52 by pguthaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char		*ft_atoi_shift_ptr(char **ptr)
+{
+	while (**ptr != '\0' && !ft_isdigit(**ptr) && **ptr != '-')
+	{
+		if (**ptr == '\e' && *(*ptr + 1) == '0')
+			return (NULL);
+		(*ptr)++;
+	}
+	if (**ptr == '\0')
+		return (NULL);
+	return (*ptr);
+}
+
+int				ft_atoi(const char *nptr)
+{
+	char		neg;
+	char		*ptr;
+	int			result;
+
+	if(!(ptr = ft_atoi_shift_ptr((char **)&nptr)))
+		return (0);
+	neg = 0;
+	if (*ptr == '-')
+	{
+		ptr++;
+		neg = 42;
+	}
+	result = 0;
+	while (*ptr && ft_isdigit(*ptr))
+	{
+		result += *ptr - '0';
+		ptr++;
+		if (*ptr && ft_isdigit(*ptr))
+			result *= 10;
+	}
+	if (neg)
+		result = -result;
+	return (result);
+}
