60441748002dad9e66f8533c8058c40a74703b52
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-11-14 16:39:35 +0100

    Hoowyye

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 15:26:58 by pguthaus          #+#    #+#             */
-/*   Updated: 2018/11/13 15:37:58 by pguthaus         ###   ########.fr       */
+/*   Updated: 2018/11/14 16:38:09 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char		*ft_strrchr(const char *s, int c)
 
 	length = ft_strlen(s);
 	tmp = (char *)s;
-	while (length)
+	while (length >= 0)
 	{
 		if (tmp[length] == c)
 			return (&tmp[length]);
