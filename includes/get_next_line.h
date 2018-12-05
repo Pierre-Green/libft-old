995c9a4b355a34c7e91a0224120f3c13d74753b1
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2018-12-05 19:29:29 +0100

    Added get_next_line to libft

---
+++ includes/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/16 19:01:40 by pguthaus          #+#    #+#             */
+/*   Updated: 2018/12/05 19:28:57 by pguthaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <limits.h>
+# include <sys/types.h>
+# include "mem.h"
+# include "io.h"
+# define BUFF_SIZE 32
+# define RET_OK 1
+# define RET_DONE 0
+# define RET_ERR -1
+
+typedef struct		s_buff
+{
+	char			content[BUFF_SIZE];
+	char			*ptr_newl;
+	size_t			content_size;
+	struct s_buff	*next;
+}					t_buff;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
---
+++ src/io/get_next_line.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/28 18:36:58 by pguthaus          #+#    #+#             */
+/*   Updated: 2018/12/05 19:24:42 by pguthaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static ssize_t		ft_len(t_buff **st_buff)
+{
+	ssize_t			len;
+	t_buff			*node;
+
+	node = (*st_buff);
+	len = 0;
+	while (node)
+	{
+		if (node->ptr_newl)
+		{
+			len += (node->ptr_newl - (char *)node->content);
+			break ;
+		}
+		len += node->content_size;
+		node = node->next;
+	}
+	return (len);
+}
+
+static ssize_t		ft_a_p(t_buff **st_bf, char *line, ssize_t current)
+{
+	char			*s;
+	ssize_t			t;
+	t_buff			*tmp;
+
+	s = (char *)(*st_bf)->content;
+	t = ((*st_bf)->ptr_newl ? ((*st_bf)->ptr_newl
+		- (char *)(*st_bf)->content) : -1);
+	ft_memmove(line + current, s, (t != -1 ? t : (*st_bf)->content_size));
+	if ((*st_bf)->ptr_newl)
+	{
+		(*st_bf)->content_size = (*st_bf)->content_size - t - 1;
+		ft_memcpy((*st_bf)->content, s + t + 1, (*st_bf)->content_size);
+		ft_bzero((char *)(*st_bf)->content + (*st_bf)->content_size,
+			BUFF_SIZE - (*st_bf)->content_size);
+		(*st_bf)->ptr_newl = ft_memchr((*st_bf)->content, '\n',
+			(*st_bf)->content_size);
+		return (-1);
+	}
+	tmp = (*st_bf);
+	(*st_bf) = (*st_bf)->next;
+	t = tmp->content_size;
+	ft_memdel((void **)&tmp);
+	return (current + t);
+}
+
+static ssize_t		ft_do_line(t_buff **st_bf, char **line)
+{
+	ssize_t			len;
+	ssize_t			current;
+
+	len = ft_len(st_bf);
+	if (!(*line = malloc(len + 1)))
+		return (RET_ERR);
+	current = 0;
+	while (*st_bf)
+		if ((current = ft_a_p(st_bf, *line, current)) == -1)
+			break ;
+	(*line)[len] = '\0';
+	return (len);
+}
+
+static t_buff		*i_buf(void)
+{
+	t_buff		*node;
+
+	if (!(node = (t_buff *)malloc(sizeof(t_buff))))
+		return (NULL);
+	node->content_size = 0;
+	node->ptr_newl = NULL;
+	node->next = NULL;
+	return (node);
+}
+
+int					get_next_line(const int fd, char **line)
+{
+	static t_buff	*s_bf[30];
+	t_buff			*node;
+	char			buff[BUFF_SIZE];
+	ssize_t			res[2];
+
+	if (read(fd, buff, 0) < 0 || !line || (!s_bf[fd] && !(s_bf[fd] = i_buf())))
+		return (RET_ERR);
+	node = s_bf[fd];
+	while (node && !(node->ptr_newl) && node->next)
+		node = node->next;
+	while (!(node->ptr_newl) && (res[0] = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		if (!(node->next = i_buf()))
+			return (RET_ERR);
+		ft_memcpy(node->content, buff, res[0]);
+		node->content_size = res[0];
+		node->ptr_newl = ft_memchr(node->content, '\n', res[0]);
+		if (node->ptr_newl)
+			break ;
+		node = node->next;
+	}
+	if ((res[0] = ft_do_line(&s_bf[fd], line)) == (int)RET_ERR ||
+		res[0] == RET_ERR || (int)res[1] == RET_ERR)
+		return (RET_ERR);
+	return (!s_bf[fd] && (*line)[0] == '\0' ? RET_DONE : RET_OK);
+}
