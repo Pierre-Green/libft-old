c919e33c83b07217346362f4cdb36cb278e53949
Pierre GUTHAUSER <pguthaus@e1r6p2.42.fr>
2019-01-15 19:20:02 +0100

    Drawable struct

--- includes/ft_mlx/ft_mlx.h
+++ includes/ft_mlx/ft_mlx.h
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   container.h                                        :+:      :+:    :+:   */
+/*   ft_mlx.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/15 17:50:52 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/01/15 19:13:03 by pguthaus         ###   ########.fr       */
+/*   Created: 2019/01/15 19:09:38 by pguthaus          #+#    #+#             */
+/*   Updated: 2019/01/15 19:16:45 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MLX_CONTAINER_H
-# define FT_MLX_CONTAINER_H
-# include <stdint.h>
-# include "ft_bool.h"
-# include "ft_mlx/ft_mlx.h"
+#ifndef FT_MLX_H
+# define FT_MLX_H
 
-typedef struct	s_container
+typedef enum		s_drawable_type
 {
-	int16_t		x;
-	int16_t		y;
-	uint16_t	width;
-	uint16_t	height;
-	t_drawable	*childs;
-	uint8_t		childs_count;
-}				t_container;
+	CONTAINER,
+	VEC2_I16
+}					t_drawable_type;
 
-t_container		*ft_init_container(int16_t x, int16_t y, uint16_t width, uint16_t height);
-
-t_bool			ft_render_container();
+typedef struct		s_drawable
+{
+	void			*data;
+	t_drawable_type	data_type;
+}					t_drawable;
 
 #endif
