1cf47d8d5146b1a002dcd77783cc4d146d69fd44
Pierre GUTHAUSER <pguthaus@student.42.fr>
2019-02-21 00:22:29 +0100

    Added window init function

--- src/mlx/window/init.c
+++ src/mlx/window/init.c
@@ -1,32 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   window.h                                           :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/20 23:15:48 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/02/21 00:21:02 by pguthaus         ###   ########.fr       */
+/*   Created: 2019/02/20 23:39:18 by pguthaus          #+#    #+#             */
+/*   Updated: 2019/02/21 00:21:40 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MLX_WINDOW_H
-# define FT_MLX_WINDOW_H
-# include <stdlib.h>
-# define DEFAULT_WIDTH 500
-# define DEFAULT_HEIGHT 1000
+#include "ft_mlx/window.h"
 
-typedef void *		t_winptr;
-
-typedef struct		s_window
+t_window			*ft_init_window()
 {
-	t_winptr		*ptr;
-	size_t			width;
-	size_t			height;
-	int				(*render)(void *);
-	struct s_window *next;
-}					t_window;
+	t_window		*win;
 
-t_window			*ft_init_window();
-
-#endif
+	if (!(win = (t_window *)malloc(sizeof(t_window))))
+		return (NULL);
+	win->render = NULL;
+	win->width = DEFAULT_WIDTH;
+	win->height = DEFAULT_HEIGHT;
+	win->ptr = NULL;
+	win->next = NULL;
+	return (win);
+}
