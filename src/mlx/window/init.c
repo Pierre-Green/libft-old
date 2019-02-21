e68351c3b441804e5b8ad7b435387b5b447ce174
pierre <pierreguthauser@gmail.com>
2019-02-21 18:50:02 +0100

    Fuck it

--- src/mlx/window/init.c
+++ src/mlx/window/init.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 23:39:18 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/02/21 01:58:27 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/02/21 18:48:39 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,26 @@ static int			null_hook()
 
 static void			add_keyboard_hook(t_window *window, int (*f)(int, void *))
 {
-	ft_lstadd(&window->keyboard_hooks, ft_lstnew(f, sizeof(f)));
+	if (window->keyboard_hooks)
+		ft_lstadd(&window->keyboard_hooks, ft_lstnew(f, sizeof(f)));
+	else
+		window->keyboard_hooks = ft_lstnew(f, sizeof(f));
 }
 
 static void			add_lkeyboard_hook(t_window *window, int (*f)(int, void *))
 {
-	ft_lstadd(&window->lkeyboard_hooks, ft_lstnew(f, sizeof(f)));
+	if (window->lkeyboard_hooks)
+		ft_lstadd(&window->lkeyboard_hooks, ft_lstnew(f, sizeof(f)));
+	else
+		window->lkeyboard_hooks = ft_lstnew(f, sizeof(f));
 }
 
 static void			add_mouse_hook(t_window *window, int (*f)(int, int, int, void *))
 {
-	ft_lstadd(&window->mouse_hooks, ft_lstnew(f, sizeof(f)));
+	if (window->mouse_hooks)
+		ft_lstadd(&window->mouse_hooks, ft_lstnew(f, sizeof(f)));
+	else
+		window->mouse_hooks = ft_lstnew(f, sizeof(f));
 }
 
 t_window			*ft_init_window()
@@ -39,11 +48,11 @@ t_window			*ft_init_window()
 	if (!(win = (t_window *)malloc(sizeof(t_window))))
 		return (NULL);
 	win->render = NULL;
-	win->keyboard_hooks = ft_lstnew(null_hook, sizeof(null_hook));;
+	win->keyboard_hooks = NULL;
 	win->add_keyboard_hook = add_keyboard_hook;
-	win->lkeyboard_hooks = ft_lstnew(null_hook, sizeof(null_hook));
+	win->lkeyboard_hooks = NULL;
 	win->add_lkeyboard_hook = add_lkeyboard_hook;
-	win->mouse_hooks = ft_lstnew(null_hook, sizeof(null_hook));;
+	win->mouse_hooks = NULL;
 	win->add_mouse_hook = add_mouse_hook;
 	win->width = DEFAULT_WIDTH;
 	win->should_render_every_frame = true;
