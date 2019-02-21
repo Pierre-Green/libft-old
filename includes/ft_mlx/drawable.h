be9506fb7e20679d97362898c0e3fbb1dec70203
Pierre GUTHAUSER <pguthaus@student.42.fr>
2019-02-21 02:54:45 +0100

    Added container drawabele

--- includes/ft_mlx/drawable.h
+++ includes/ft_mlx/drawable.h
@@ -6,18 +6,17 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 02:14:46 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/02/21 02:36:14 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/02/21 02:53:47 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MLX_DRAWABLE_H
 # define FT_MLX_DRAWABLE_H
 # include "ft_math/point.h"
-# include "ft_mlx/button.h"
-# include "ft_mlx/text.h"
 
 typedef enum			e_drawable_types
 {
+						CONTAINER,
 						TEXT,
 						BUTTON,
 						NONE
--- src/mlx/drawable/render.c
+++ src/mlx/drawable/render.c
@@ -6,14 +6,19 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 02:23:35 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/02/21 02:35:52 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/02/21 02:54:20 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mlx/drawable.h"
+#include "ft_mlx/text.h"
+#include "ft_mlx/button.h"
+#include "ft_mlx/container.h"
 
 int						ft_drawable_routerender(t_drawable drawable, t_point2d offset, void *s)
 {
+	if (drawable.type == CONTAINER)
+		return (((t_container *)drawable.drawable)->draw(drawable.drawable, offset, s));
 	if (drawable.type == TEXT)
 		return (((t_text *)drawable.drawable)->draw(drawable.drawable, offset, s));
 	if (drawable.type == BUTTON)
