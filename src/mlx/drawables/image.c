e90a142b2bb36bb11bcbf2d5064e768695400f31
pierre <pierreguthauser@gmail.com>
2019-02-25 17:47:00 +0100

    yess

--- src/mlx/drawables/image.c
+++ src/mlx/drawables/image.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 17:13:54 by pierre            #+#    #+#             */
-/*   Updated: 2019/02/25 17:27:05 by pierre           ###   ########.fr       */
+/*   Updated: 2019/02/25 17:46:49 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ t_image_carry			*ft_image_merge(t_image_carry *carry, t_point2d offset, char *va
 	return (NULL);
 }
 
-void						ft_put_pixel_to_image(char *data, int sl, int bpp, t_point2d pos)
+void					ft_put_pixel_to_image(t_image_carry *carry, size_t x, size_t y)
 {
 	unsigned int		color;
+	const ssize_t		offset = ((y * carry->size_line) + (x *(carry->bits_per_pixels / 8)));
 
-	color = 0xFF0000;
-	data[pos.y * sl + pos.x * (bpp / 8)] = (color & 0xFF);
-  	data[(pos.y * sl + pos.x * (bpp / 8)) + 1] = (color & 0xFF00) >> 8;
- 	data[(pos.y * sl + pos.x * (bpp / 8)) + 2] = (color & 0xFF0000) >> 16;
+	color = 0xFFFFFF;
+	(*carry->data)[offset] = (color & 0xFF);
+  	(*carry->data)[offset + 1] = (color & 0xFF00) >> 8;
+ 	(*carry->data)[offset + 2] = (color & 0xFF0000) >> 16;
 }
