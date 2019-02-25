2989f4cd790042a95af5f511a8c993cf8217792c
pierre <pierreguthauser@gmail.com>
2019-02-25 17:27:26 +0100

    Logical

--- src/mlx/drawables/image.c
+++ src/mlx/drawables/image.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 17:13:54 by pierre            #+#    #+#             */
-/*   Updated: 2019/02/22 17:22:56 by pierre           ###   ########.fr       */
+/*   Updated: 2019/02/25 17:27:05 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 t_image_carry			*ft_image_merge(t_image_carry *carry, t_point2d offset, char *value)
 {
-	
+	(void)value;
+	(void)offset;
+	(void)carry;
+	return (NULL);
+}
+
+void						ft_put_pixel_to_image(char *data, int sl, int bpp, t_point2d pos)
+{
+	unsigned int		color;
+
+	color = 0xFF0000;
+	data[pos.y * sl + pos.x * (bpp / 8)] = (color & 0xFF);
+  	data[(pos.y * sl + pos.x * (bpp / 8)) + 1] = (color & 0xFF00) >> 8;
+ 	data[(pos.y * sl + pos.x * (bpp / 8)) + 2] = (color & 0xFF0000) >> 16;
 }
--- src/mlx/drawables/utils.c
+++ src/mlx/drawables/utils.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 14:30:16 by pierre            #+#    #+#             */
-/*   Updated: 2019/02/25 15:44:53 by pierre           ###   ########.fr       */
+/*   Updated: 2019/02/25 17:25:51 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,3 @@ t_image_carry				*ft_image_carry_from(char **addr, int bpp, int sizel, int endia
 	return (carry);
 }
 
-void						ft_put_pixel_to_image(char *data, int sl, int bpp, t_point2d pos)
-{
-	unsigned int		color;
-
-	color = 0xFF0000;
-	data[pos.y * sl + pos.x * (bpp / 8)] = (color & 0xFF);
-  	data[(pos.y * sl + pos.x * (bpp / 8)) + 1] = (color & 0xFF00) >> 8;
- 	data[(pos.y * sl + pos.x * (bpp / 8)) + 2] = (color & 0xFF0000) >> 16;
-}
