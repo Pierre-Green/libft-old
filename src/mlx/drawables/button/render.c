534b5e37bbc030fc168493cdb7274ecaaf934178
pierre <pierreguthauser@gmail.com>
2019-03-07 15:30:08 +0100

    Baselie adjusment

--- src/mlx/drawables/button/render.c
+++ src/mlx/drawables/button/render.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:11:32 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/07 14:14:37 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/07 15:29:59 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ void					mlx_button_render_txt(t_button *button, t_point2d offset, void *mlx_ptr
 {
 	const t_window		*window = win;
 
-	mlx_string_put(mlx_ptr, window->ptr, offset.x + button->pos.x, offset.y + button->pos.y, 0xFFFFFF, button->text);
+	mlx_string_put(mlx_ptr, window->ptr, offset.x + button->pos.x, offset.y + button->pos.y + button->dim.height, 0xFFFFFF, button->text);
 }
