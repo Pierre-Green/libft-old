248f7b4aaef05acbffc4280d9fcb13d2834cfc6b
pierre <pierreguthauser@gmail.com>
2019-03-07 03:24:33 +0100

    string render

--- src/mlx/drawables/container/render.c
+++ src/mlx/drawables/container/render.c
@@ -6,13 +6,32 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 02:48:40 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/07 03:16:38 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/07 03:24:13 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mlx/drawables.h"
 #include "ft_mlx/window.h"
 #include <mlx.h>
+
+static void					mlx_container_text_drawer(t_container *container, void *mlx_ptr, t_window *win)
+{
+	t_drawables				*node;
+	t_text					*txt;
+
+	node = container->childs;
+	while (node)
+	{
+		if (node->drawable->type == CONTAINER)
+			mlx_container_text_drawer(node->drawable->drawable, mlx_ptr, win);
+		if (node->drawable->type == TEXT)
+		{
+			txt = (t_text *)node->drawable->drawable;
+			mlx_string_put(mlx_ptr, win->ptr, txt->pos.x, txt->pos.y, txt->color, txt->text);
+		}
+		node = node->next;
+	}
+}
 
 t_image_carry				*mlx_container_render(t_container *container, void *mlx_ptr, void *win, t_image_carry *old)
 {
@@ -29,5 +48,6 @@ t_image_carry				*mlx_container_render(t_container *container, void *mlx_ptr, vo
 	}
 	*old->data = container->image(container, POS(0, 0), old);
 	mlx_put_image_to_window(mlx_ptr, window->ptr, old->img_ptr, 0, 0);
+	mlx_container_text_drawer(container, mlx_ptr, (t_window *)window);
 	return (old);
 }
