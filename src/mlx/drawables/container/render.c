f07a9a027e689df7e28cb9c113e4d32058ebcf26
pierre <pierreguthauser@gmail.com>
2019-03-07 03:12:21 +0100

    try fix

--- src/mlx/drawables/container/render.c
+++ src/mlx/drawables/container/render.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 02:48:40 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/07 02:58:57 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/07 03:11:38 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_image_carry				*mlx_container_render(t_container *container, void *mlx_ptr, vo
 		tmpch = mlx_get_data_addr(tmptr, &params[0], &params[1], &params[2]);
 		old = ft_image_carry_from(&tmpch, params[0], params[1], params[2]);
 	}
-	container->image(container, POS(0, 0), old);
+	*old->data = container->image(container, POS(0, 0), old);
 	mlx_put_image_to_window(mlx_ptr, window->ptr, old->data, 0, 0);
 	return (old);
 }
