fcbc841535997cb97c22a4f58cf98c18c7507f45
pierre <pierreguthauser@gmail.com>
2019-03-07 02:01:11 +0100

    fixed

--- src/mlx/drawables/container/background.c
+++ src/mlx/drawables/container/background.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 01:22:47 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/07 01:26:33 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/07 02:00:37 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	mlx_container_background(t_container *container, t_point2d offset, t_image_
 	size_t			y;
 
 	y = offset.y + container->pos.y;
-	while ((y - offset.y) < container->dim.height)
+	while ((y - (offset.y + container->pos.y)) < container->dim.height)
 	{
 		x = offset.x + container->pos.x;
-		while ((x - offset.x) < container->dim.width)
+		while ((x - (offset.x + container->pos.x)) < container->dim.width)
 		{
 			ft_put_pixel_to_image(carry, x, y);
 			x++;
