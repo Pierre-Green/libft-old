2a095d72a98b1d297c0a6100a9a9d2a402c54fb0
pierre <pierreguthauser@gmail.com>
2019-03-08 01:49:11 +0100

    Fixed

--- src/mlx/drawables/pagination/init.c
+++ src/mlx/drawables/pagination/init.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 17:43:14 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/07 19:30:06 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/08 01:48:48 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ t_pagination			*mlx_init_pagination(t_zone2d zone, t_dim2d item_dim, t_margin it
 		pagination->items_margin[curr - 1] = item_margin[curr - 1];
 	pagination->items_count = 0;
 	pagination->items = NULL;
+	pagination->render_txt = mlx_pagination_render_txt;
 	pagination->add_child = pagination_add_item;
 	pagination->image = mlx_pagination_image;
 	return (pagination);
