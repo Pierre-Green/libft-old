afeab319e58f7d106272f66fe8b10b0ad7f0c2ca
pierre <pierreguthauser@gmail.com>
2019-03-08 02:06:03 +0100

    test

--- src/mlx/drawables/pagination/utils.c
+++ src/mlx/drawables/pagination/utils.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/08 01:41:58 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/08 02:03:18 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/08 02:05:32 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,7 @@ static t_point2d		pagination_center(t_pagination *pagination, t_point2d pos, t_d
 
 	overflow.width = pagination->zone.dim.width
 		- (griddim.width * (pagination->items_dim.width + pagination->items_margin[3] + pagination->items_margin[1]));
-	overflow.height = pagination->zone.dim.height
-		- (griddim.height * (pagination->items_dim.height + pagination->items_margin[0] + pagination->items_margin[2]));
 	pos.x += overflow.width / 2;
-	pos.y += overflow.width / 2;
 	return (pos);
 }
 
