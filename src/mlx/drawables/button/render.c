fa5fc4dc6daf9f3706b35739c5af0a08a975d664
pierre <pierreguthauser@gmail.com>
2019-03-10 00:32:17 +0100

    Added temporary button text placement

--- src/mlx/drawables/button/render.c
+++ src/mlx/drawables/button/render.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:11:32 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/10 00:27:15 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/10 00:32:05 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void					mlx_button_render_txt(t_button *button, t_point2d offset, void *mlx_ptr
 
 	window = win;
 	window->add_mouse_hook(window, button->uuid, ft_zone2d_from_pdim(DDSUM(offset, button->pos), button->dim), button->onclick);
-	mlx_string_put(mlx_ptr, window->ptr, offset.x + button->pos.x, offset.y + button->pos.y + button->dim.height, 0xFFFFFF, button->text);
+	mlx_string_put(mlx_ptr, window->ptr, offset.x + button->pos.x + button->dim.width / 2, offset.y + button->pos.y + button->dim.height / 2, 0xFFFFFF, button->text);
 }
