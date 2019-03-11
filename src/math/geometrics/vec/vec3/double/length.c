c54a2ce9bd24ae32280e96346d75210dcfeb20ac
Pierre GUTHAUSER <pguthaus@student.42.fr>
2019-03-11 22:38:26 +0100

    Fixed vec

--- src/math/geometrics/vec/vec3/double/length.c
+++ src/math/geometrics/vec/vec3/double/length.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 15:50:11 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/01/14 19:33:18 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/03/11 22:33:17 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 double		ft_vec3_d_length(t_vec3_d self)
 {
-	return ((int8_t)round(sqrt(ft_vec3_d_norm(self))));
+	return (sqrt(ft_vec3_d_norm(self)));
 }
