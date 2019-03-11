031efad34dbc08d29ec1be3ccef1c79849b20f33
Pierre GUTHAUSER <pguthaus@student.42.fr>
2019-03-11 19:18:58 +0100

    Text centered

--- src/mlx/drawables/button/render.c
+++ src/mlx/drawables/button/render.c
@@ -6,19 +6,22 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:11:32 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/10 00:32:05 by pierre           ###   ########.fr       */
+/*   Updated: 2019/03/11 19:18:39 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mlx/drawables.h"
 #include "ft_mlx/window.h"
+#include "ft_str.h"
 #include <mlx.h>
 
 void					mlx_button_render_txt(t_button *button, t_point2d offset, void *mlx_ptr, void *win)
 {
 	t_window			*window;
+	size_t				text_width;
 
 	window = win;
+	text_width = ft_strlen(button->text) * (10 + 2);
 	window->add_mouse_hook(window, button->uuid, ft_zone2d_from_pdim(DDSUM(offset, button->pos), button->dim), button->onclick);
-	mlx_string_put(mlx_ptr, window->ptr, offset.x + button->pos.x + button->dim.width / 2, offset.y + button->pos.y + button->dim.height / 2, 0xFFFFFF, button->text);
+	mlx_string_put(mlx_ptr, window->ptr, (offset.x + button->pos.x + button->dim.width / 2) - text_width / 2, offset.y + button->pos.y + button->dim.height / 2, 0xFFFFFF, button->text);
 }
