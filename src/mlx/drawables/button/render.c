8c393522b014cfedfbe0021a86def32bb1d85f11
Pierre GUTHAUSER <pguthaus@student.42.fr>
2019-03-12 14:46:39 +0100

    Butten with centered text

--- src/mlx/drawables/button/render.c
+++ src/mlx/drawables/button/render.c
@@ -6,7 +6,7 @@
 /*   By: pierre </var/spool/mail/pierre>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:11:32 by pierre            #+#    #+#             */
-/*   Updated: 2019/03/11 20:07:01 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/03/12 14:45:57 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void					mlx_button_render_txt(t_button *button, t_point2d offset, void *win)
 	t_point2d			next_offset;
 
 	next_offset = DDSUM(offset, button->pos);
+	next_offset.y += button->dim.height / 2;
+	next_offset.x += button->dim.width / 2;
 	window = win;
 	window->add_mouse_hook(window, button->uuid, ft_zone2d_from_pdim(DDSUM(offset, button->pos), button->dim), button->onclick);
 	button->text->render_txt(button->text, next_offset, win);
