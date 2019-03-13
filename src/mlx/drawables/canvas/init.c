d9868d39ee1a845fa1b3e5c914e24fcd7ac3b18e
Pierre GUTHAUSER <pguthaus@student.42.fr>
2019-03-13 19:51:21 +0100

    Still trying to fix

--- src/mlx/drawables/canvas/init.c
+++ src/mlx/drawables/canvas/init.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 16:41:15 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/03/12 19:04:51 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/03/13 19:51:05 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ t_canvas				*mlx_init_canvas(t_point2d pos, t_dim2d dim, t_image_carry *(*image)
 	canvas->s = s;
 	canvas->zone = ft_zone2d_from_pdim(pos, dim);
 	canvas->image = image;
+	canvas->image_intern = mlx_canvas_image_intern;
 	img_ptr = mlx_new_image(mlx_ptr, dim.width, dim.height);
 	data = mlx_get_data_addr(img_ptr, &params[0], &params[1], &params[2]);
 	canvas->img = ft_image_carry_from(img_ptr, &data, params[0], params[1], params[2]);
