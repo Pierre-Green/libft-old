54e88d7a36394922edaa0daba50ca930090d71b9
pguthaus <pguthaus@student.42.fr>
2019-03-22 19:31:37 +0100

    Fixed matrix initialization

--- src/math/geometrics/matrix/44/double/init.c
+++ src/math/geometrics/matrix/44/double/init.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 19:15:31 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/03/18 17:01:38 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/03/22 19:31:14 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,19 @@
 
 static void					fill_with(t_matrix44_d *matrix, double a)
 {
-	uint8_t					x;
-	uint8_t					y;
-
-	y = 0;
-	while (y <= 4)
-	{
-		x = 0;
-		while (++x <= 4)
-			matrix->matrix[y][x - 1] = a;
-		y++;
-	}
+	*matrix = (t_matrix44_d)(t_mat44_d_data){
+		a, a, a, a,
+		a, a, a, a,
+		a, a, a, a,
+		a, a, a, a
+	};
 }
 
 t_matrix44_d				ft_init0_matrix44_d(void)
 {
 	t_matrix44_d			matrix;
 
-	fill_with(&matrix, 0.0);
+	fill_with(&matrix, 0);
 	return (matrix);
 }
 
