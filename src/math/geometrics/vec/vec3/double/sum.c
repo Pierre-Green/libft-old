8397987447cbbb3397a21e442a77854dd44936ad
pguthaus <pguthaus@student.42.fr>
2019-03-22 19:58:27 +0100

    Fixed vec3d sum

--- src/math/geometrics/vec/vec3/double/sum.c
+++ src/math/geometrics/vec/vec3/double/sum.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 15:32:08 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/01/14 19:37:18 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/03/22 19:58:13 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_vec3_d		ft_vec3_d_sum(t_vec3_d self, t_vec3_d vec)
 {
 	self.x += vec.x;
+	self.z += vec.z;
 	self.y += vec.y;
 	return (self);
 }
