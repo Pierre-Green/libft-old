0f0f9dbe4e392be848512049365e8cf3967b6b27
pguthaus <pguthaus@student.42.fr>
2019-04-01 17:44:12 +0200

    Updated cam mat

--- src/mlx/camera/update.c
+++ src/mlx/camera/update.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:52:15 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/03/25 20:05:13 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/01 17:43:15 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,12 @@ void					mlx_camera_update(t_camera *self)
 
 void					mlx_camera_update_view_mat(t_camera *self)
 {
-	t_matrix44_d		mat1;
-	t_matrix44_d		mat2;
-
-	mat1 = ft_init44_matrix44_d((t_mat44_d_data){
-			self->vec_right.x, self->vec_right.y, self->vec_right.z, 0,
-			self->vec_up.x, self->vec_up.y, self->vec_up.z, 0,
-			self->vec_front.x, self->vec_front.y, self->vec_front.z, 0,
-			0, 0, 0, 1
-	});
-	mat2 = ft_init44_matrix44_d((t_mat44_d_data){
-			1, 0, 0, -(self->position.x),
-			0, 1, 0, -(self->position.y),
-			0, 0, 1, -(self->position.z),
-			0, 0, 0, 1
-	});
-	self->view_mat = ft_multiply_matrix44_d(mat1, mat2);
+	self->view_mat.raw = (t_mat44_d_data){
+		self->vec_right.x, self->vec_right.y, self->vec_right.z, 0,
+		self->vec_up.x, self->vec_up.y, self->vec_up.z, 0,
+		self->vec_front.x, self->vec_front.y, self->vec_front.z, 0,
+		self->position.x, self->position.y, self->position.z, 1
+	};
 }
 
 void					mlx_camera_update_vecs(t_camera *self)
