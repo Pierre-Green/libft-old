5610958ed9141ffbcd7ae98ff3881f89c7c0eea1
pguthaus <pguthaus@student.42.fr>
2019-04-01 18:37:37 +0200

    Added camera rotation limits

--- src/mlx/camera/mouse.c
+++ src/mlx/camera/mouse.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 17:08:04 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/04/01 17:32:21 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/01 18:37:22 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ void					mlx_camera_mouse(t_camera *self, double xoffset, double yoffset)
 {
 	self->yaw += xoffset * self->mouse_velocity;
 	self->pitch += yoffset * self->mouse_velocity;
+	if (self->yaw > 89)
+		self->yaw = 89;
+	if (self->pitch > 89)
+		self->pitch = 89;
 	mlx_camera_update_vecs(self);
 }
