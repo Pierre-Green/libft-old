8401cd80e9127bfc1231d1885abfb462adc845f7
pguthaus <pguthaus@student.42.fr>
2019-04-01 21:45:47 +0200

    Fixed

--- src/utils/ft_int_from_hexstr.c
+++ src/utils/ft_int_from_hexstr.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 21:11:03 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/04/01 21:26:21 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/01 21:45:34 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ uintmax_t		ft_int_from_hexstr(char *str)
 		if (str[current] >= '0' && str[current] <= '9')
 			res += (str[current] - '0') * (1 << (4 * (len - 1 - current)));
 		else if (str[current] >= 'A' && str[current] <= 'F')
-			res += (str[current] - 'A') * (1 << (4 * (len - 1 - current)));
+			res += (str[current] - 55) * (1 << (4 * (len - 1 - current)));
 		else
 			return (0);
 		current++;
