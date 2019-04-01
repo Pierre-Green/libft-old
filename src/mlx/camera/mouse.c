646e6da06fa3c4af11ae2e0cfd55457345c1a5a8
pguthaus <pguthaus@student.42.fr>
2019-04-01 17:35:20 +0200

    Fixed pitch yaw cam

--- src/mlx/camera/mouse.c
+++ src/mlx/camera/mouse.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 17:08:04 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/03/18 18:09:40 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/01 17:32:21 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void					mlx_camera_mouse(t_camera *self, double xoffset, double yoffset)
 {
-	self->yaw = xoffset * self->mouse_velocity;
-	self->pitch = yoffset * self->mouse_velocity;
+	self->yaw += xoffset * self->mouse_velocity;
+	self->pitch += yoffset * self->mouse_velocity;
 	mlx_camera_update_vecs(self);
 }
