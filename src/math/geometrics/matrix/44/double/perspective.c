1cb061e132c3f33a76095d5ee0f4d41c26df6c5d
pguthaus <pguthaus@student.42.fr>
2019-04-02 17:03:49 +0200

    Perspective matrix try

--- src/math/geometrics/matrix/44/double/perspective.c
+++ src/math/geometrics/matrix/44/double/perspective.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 17:29:59 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/04/01 17:30:56 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/02 17:03:32 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ t_matrix44_d			ft_perspective_matrix44_d(double fov, double near, double far)
 	double				a;
 	double				b;
 
-	scale = 1 / tan(fov / 2 * M_PI / 180);
+	scale = 1 / tan((fov / 2) * M_PI / 180);
 	a = -(far / (far - near));
 	b = -(far * near / (far - near));
 	return (ft_init44_matrix44_d((t_mat44_d_data){
 		scale, 0, 0, 0,
-		0, scale, 0, 0,
+		0, -scale, 0, 0,
 		0, 0, a, -1,
 		0, 0, b, 0
 	}));
