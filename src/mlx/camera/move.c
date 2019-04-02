63e91b445520baedcde17f48fba6affbbcf77022
pguthaus <pguthaus@student.42.fr>
2019-04-02 18:18:13 +0200

    Hey

--- src/mlx/camera/move.c
+++ src/mlx/camera/move.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 17:07:42 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/04/02 18:10:07 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/02 18:18:03 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 void					mlx_camera_move(t_camera *self, t_camera_movement movement)
 {
 	if (movement == FORWARD)
-		self->position = ft_vec3_d_minus(self->position,
+		self->position = ft_vec3_d_sum(self->position,
 			ft_vec3_d_product1(self->vec_front, self->velocity));
 	else if (movement == BACKWARD)
-		self->position = ft_vec3_d_sum(self->position,
+		self->position = ft_vec3_d_minus(self->position,
 			ft_vec3_d_product1(self->vec_front, self->velocity));
 	else if (movement == RIGHTWARD)
-		self->position = ft_vec3_d_minus(self->position,
+		self->position = ft_vec3_d_sum(self->position,
 			ft_vec3_d_normalize(ft_vec3_d_product1(self->vec_right, self->velocity)));
 	else if (movement == LEFTWARD)
-		self->position = ft_vec3_d_sum(self->position,
+		self->position = ft_vec3_d_minus(self->position,
 			ft_vec3_d_normalize(ft_vec3_d_product1(self->vec_right, self->velocity)));
 	self->update(self);
 }
