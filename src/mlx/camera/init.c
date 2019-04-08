f71e875eeb758fcb74c0941d7652d29537a2e379
pguthaus <pguthaus@student.42.fr>
2019-04-08 18:23:45 +0200

    Fixed camera init rotation

--- src/mlx/camera/init.c
+++ src/mlx/camera/init.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:49:46 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/04/08 17:46:36 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/04/08 18:23:17 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ t_camera				*mlx_init_camera(t_vec3_d pos)
 	if (!(camera = malloc(sizeof(t_camera))))
 		return (NULL);
 	camera->position = pos;
-	camera->rotation.y = CAM_DEFAULT_YAW;
-	camera->rotation.x = CAM_DEFAULT_PITCH;
+	camera->rotation = (t_vec3_d){ CAM_DEFAULT_PITCH, CAM_DEFAULT_YAW, 0 };
 	camera->trans_velocity = CAM_DEFAULT_TRANSLATION_VELOCITY;
 	camera->mode = PERSPECTIVE;
 	camera->rotat_velocity = CAM_DEFAULT_ROTATION_VELOCITY;
